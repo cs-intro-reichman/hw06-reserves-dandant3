8f6ce80056ee9db3deeba0463a649b7b1ad0f692
Dandan <dandant3@gmail.com>
2024-02-28 09:48:17 +0200

    final

--- Editor4.java
+++ Editor4.java
@@ -6,7 +6,7 @@ public class Editor4 {
 		String source = args[0];
 		int n = Integer.parseInt(args[1]);
 		Color[][] sourceImage = Runigram.read(source);
-		Color[][] grayscaledImage = grayScaled(sourceImage);
+		Color[][] grayscaledImage = Runigram.grayScaled(sourceImage);
 		Runigram.setCanvas(sourceImage);
         Runigram.morph(sourceImage, grayscaledImage, n);
 	}
